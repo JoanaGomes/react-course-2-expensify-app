e7ffa7a2481ae90c2778cbc6dc23d7e5ea07b14e
Joana Gomes <joanalourinhogomes@gmail.com>
2018-02-14 15:56:55 +0000

    fix: switch state to dispatch as constructor variable on mapDispatchToProps on ExpenseListFilters; setup sourcemaps

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -59,7 +59,7 @@ const mapStateToProps = (state) => ({
     filters: state.filters
 })
 
-const mapDispatchToProps = (state) => ({
+const mapDispatchToProps = (dispatch) => ({
   setTextFilter: (text) => dispatch(setTextFilter(text)),
   sortByDate: () => dispatch(sortByDate()),
   sortByAmount: () => dispatch(sortByAmount()),
